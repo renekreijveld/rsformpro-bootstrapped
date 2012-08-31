2fae83a635a78e61992bee8a7123db54912f39a6
Rene Kreijveld <email@renekreijveld.nl>
2012-08-31 16:10:53 +0200

    updated jquery.rsformswitch.js

--- templates/yourtemplate/js/jquery.rsformswitch.js
+++ templates/yourtemplate/js/jquery.rsformswitch.js
@@ -1,25 +1,25 @@
 /* change layout RSForm from horizontal to vertical */
 
-(function($){
-	$(document).ready(function() {
+(function(jQuery){
+	jQuery(document).ready(function() {
 		var SWidth = $(window).width();
 		if (SWidth < 766) {
-			$("form#userForm").removeClass("form-horizontal");
-			$("form#userForm").addClass("form-vertical");
+			jQuery("form#userForm").removeClass("form-horizontal");
+			jQuery("form#userForm").addClass("form-vertical");
 		}
 		if (SWidth >= 766) {
-			$("form#userForm").removeClass("form-vertical");
-			$("form#userForm").addClass("form-horizontal");
+			jQuery("form#userForm").removeClass("form-vertical");
+			jQuery("form#userForm").addClass("form-horizontal");
 		}
-		$(window).resize(function() {
-			var ScreenWidth = $(window).width();
+		jQuery(window).resize(function() {
+			var ScreenWidth = jQuery(window).width();
 			if (ScreenWidth < 766) {
-				$("form#userForm").removeClass("form-horizontal");
-				$("form#userForm").addClass("form-vertical");
+				jQuery("form#userForm").removeClass("form-horizontal");
+				jQuery("form#userForm").addClass("form-vertical");
 			}
 			if (ScreenWidth >= 766) {
-				$("form#userForm").removeClass("form-vertical");
-				$("form#userForm").addClass("form-horizontal");
+				jQuery("form#userForm").removeClass("form-vertical");
+				jQuery("form#userForm").addClass("form-horizontal");
 			}
 		});
 	});
