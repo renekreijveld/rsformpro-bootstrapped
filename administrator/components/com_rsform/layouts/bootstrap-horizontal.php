e5c1f1fec59b53e6a29f63981346afbffbad8f64
Rene Kreijveld <email@renekreijveld.nl>
2012-10-09 10:47:19 +0200

    Hidden fields hack
    Bootstrap layouts modified. If a fieldname ends with the text "_hidden"
    then the Bottstrap layouts will just add the {body} code for that field
    without the surrounding divs.

--- administrator/components/com_rsform/layouts/bootstrap-horizontal.php
+++ administrator/components/com_rsform/layouts/bootstrap-horizontal.php
@@ -43,13 +43,17 @@ foreach ($quickfields as $quickfield)
 	}
 	
 	$required = in_array($quickfield, $requiredfields) ? ' '.(isset($this->_form->Required) ? $this->_form->Required : '(*)') : "";
-	$out.= "\t".'<div class="control-group rsform-block rsform-block-'.JFilterOutput::stringURLSafe($quickfield).'">'."\n";
-	$out.= "\t\t<label class=\"control-label\" for=\"".$quickfield."\">{".$quickfield.":caption}".$required."</label>\n";
-	$out.= "\t\t<div class=\"controls\">\n";
-	$out.= "\t\t\t{".$quickfield.":body}&nbsp;{".$quickfield.":validation}\n";
-	$out.= "\t\t\t<span class=\"help-block\">{".$quickfield.":description}</span>\n";
-	$out.= "\t\t</div>\n";
-	$out.= "\t</div>\n";
+	if (strtolower(substr($quickfield,-7)) == "_hidden") {
+		$out.= "{".$quickfield.":body}\n";
+	} else {
+		$out.= "\t".'<div class="control-group rsform-block rsform-block-'.JFilterOutput::stringURLSafe($quickfield).'">'."\n";
+		$out.= "\t\t<label class=\"control-label\" for=\"".$quickfield."\">{".$quickfield.":caption}".$required."</label>\n";
+		$out.= "\t\t<div class=\"controls\">\n";
+		$out.= "\t\t\t{".$quickfield.":body}&nbsp;{".$quickfield.":validation}\n";
+		$out.= "\t\t\t<span class=\"help-block\">{".$quickfield.":description}</span>\n";
+		$out.= "\t\t</div>\n";
+		$out.= "\t</div>\n";
+	}
 }
 if (!empty($pagefields))
 	$out .= "\t".'</div>'."\n";
