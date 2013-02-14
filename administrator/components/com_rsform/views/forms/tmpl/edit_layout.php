c5530211dd8a360e27b9f5fff5b36be61857c456
Hans Kuijpers <hans2103@gmail.com>
2013-02-14 12:14:12 +0100

    replaced entire file with newest file from RSForms Pro. Too much code has been changed.

--- administrator/components/com_rsform/views/forms/tmpl/edit_layout.php
+++ administrator/components/com_rsform/views/forms/tmpl/edit_layout.php
@@ -10,75 +10,45 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 <fieldset>
 	<legend><?php echo JText::_('RSFP_CLASSIC_LAYOUTS'); ?></legend>
+	<?php $classicLayouts = array('inline', '2lines', '2colsinline', '2cols2lines'); ?>
+	<?php foreach ($classicLayouts as $layout) { ?>
 	<div class="rsform_layout_box">
-		<label for="formLayoutInline">
-			<input type="radio" id="formLayoutInline" name="FormLayoutName" value="inline" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','inline');" <?php if ($this->form->FormLayoutName == 'inline') echo 'checked="checked"'; ?> /><?php echo JText::_('RSFP_LAYOUT_INLINE');?><br/>
-			<img src="components/com_rsform/assets/images/layouts/inline.gif" width="175"/>
+		<label for="formLayout<?php echo ucfirst($layout); ?>">
+			<input type="radio" id="formLayout<?php echo ucfirst($layout); ?>" name="FormLayoutName" value="<?php echo $layout; ?>" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>', this.value);" <?php if ($this->form->FormLayoutName == $layout) { ?>checked="checked"<?php } ?> /><?php echo JText::_('RSFP_LAYOUT_'.$layout);?><br/>
+			<img src="components/com_rsform/assets/images/layouts/<?php echo $layout; ?>.gif" width="175"/>
 		</label>
 	</div>
-	<div class="rsform_layout_box">
-		<label for="formLayout2lines">
-			<input type="radio" id="formLayout2lines" name="FormLayoutName" value="2lines" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','2lines')" <?php if ($this->form->FormLayoutName == '2lines') echo 'checked="checked"'; ?>/><?php echo JText::_('RSFP_LAYOUT_2LINES');?><br/>
-			<img src="components/com_rsform/assets/images/layouts/2lines.gif" width="175"/>
-		</label>
-	</div>
-	<div class="rsform_layout_box">
-		<label for="formLayout2colsinline">
-			<input type="radio" id="formLayout2colsinline" name="FormLayoutName" value="2colsinline" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','2colsinline')" <?php if ($this->form->FormLayoutName == '2colsinline') echo 'checked="checked"'; ?> /><?php echo JText::_('RSFP_LAYOUT_2COLSINLINE');?><br/>
-			<img src="components/com_rsform/assets/images/layouts/2colsinline.gif" width="175"/>
-		</label>
-	</div>
-	<div class="rsform_layout_box">
-		<label for="formLayout2cols2lines">
-			<input type="radio" id="formLayout2cols2lines" name="FormLayoutName" value="2cols2lines" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','2cols2lines')" <?php if ($this->form->FormLayoutName == '2cols2lines') echo 'checked="checked"'; ?>/><?php echo JText::_('RSFP_LAYOUT_2COLS2LINES');?><br/>
-			<img src="components/com_rsform/assets/images/layouts/2cols2lines.gif" width="175"/>
-		</label>
-	</div>
+	<?php } ?>
 	<span class="rsform_clear_both"></span>
 </fieldset>
 
 <fieldset>
 	<legend><?php echo JText::_('RSFP_XHTML_LAYOUTS'); ?></legend>
+	<?php $xhtmlLayouts = array('inline-xhtml', '2lines-xhtml', 'responsive'); ?>
+	<?php foreach ($xhtmlLayouts as $layout) { ?>
 	<div class="rsform_layout_box">
-		<label for="formLayoutInlineXhtml">
-			<input type="radio" id="formLayoutInlineXhtml" name="FormLayoutName" value="inline-xhtml" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','inline-xhtml');" <?php if ($this->form->FormLayoutName == 'inline-xhtml') echo 'checked="checked"'; ?> /><?php echo JText::_('RSFP_LAYOUT_INLINE_XHTML');?><br/>
-			<img src="components/com_rsform/assets/images/layouts/inline-xhtml.gif" width="175"/>
+		<label for="formLayout<?php echo ucfirst($layout); ?>">
+			<input type="radio" id="formLayout<?php echo ucfirst($layout); ?>" name="FormLayoutName" value="<?php echo $layout; ?>" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>', this.value);" <?php if ($this->form->FormLayoutName == $layout) { ?>checked="checked"<?php } ?> /><?php echo JText::_('RSFP_LAYOUT_'.str_replace('-', '_', $layout));?><br/>
+			<img src="components/com_rsform/assets/images/layouts/<?php echo $layout; ?>.gif" width="175"/>
 		</label>
 	</div>
-	<div class="rsform_layout_box">
-		<label for="formLayout2linesXhtml">
-			<input type="radio" id="formLayout2linesXhtml" name="FormLayoutName" value="2lines-xhtml" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','2lines-xhtml')" <?php if ($this->form->FormLayoutName == '2lines-xhtml') echo 'checked="checked"'; ?>/><?php echo JText::_('RSFP_LAYOUT_2LINES_XHTML');?><br/>
-			<img src="components/com_rsform/assets/images/layouts/2lines-xhtml.gif" width="175"/>
-		</label>
-	</div>
-	<div class="rsform_layout_box">
-		<label for="formLayoutResponsive">
-			<input type="radio" id="formLayoutResponsive" name="FormLayoutName" value="responsive" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','responsive')" <?php if ($this->form->FormLayoutName == 'responsive') echo 'checked="checked"'; ?>/><?php echo JText::_('RSFP_LAYOUT_RESPONSIVE');?><br/>
-			<img src="components/com_rsform/assets/images/layouts/responsive.gif" width="175"/>
-		</label>
-	</div>
-	<div class="rsform_layout_box">
-		<label for="formLayoutBootstrapVertical">
-			<input type="radio" id="formLayoutBootstrapVertical" name="FormLayoutName" value="bootstrap-vertical" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','bootstrap-vertical')" <?php if ($this->form->FormLayoutName == 'bootstrap-vertical') echo 'checked="checked"'; ?>/>Bootstrap Vertical<br/>
-			<img src="components/com_rsform/assets/images/layouts/bootstrap-vertical.gif" width="175"/>
-		</label>
-	</div>
-	<div class="rsform_layout_box">
-		<label for="formLayoutBootstrapHorizontal">
-			<input type="radio" id="formLayoutBootstrapHorizontal" name="FormLayoutName" value="bootstrap-horizontal" onclick="saveLayoutName('<?php echo $this->form->FormId; ?>','bootstrap-horizontal');" <?php if ($this->form->FormLayoutName == 'bootstrap-horizontal') echo 'checked="checked"'; ?> />Bootstrap Horizontal<br/>
-			<img src="components/com_rsform/assets/images/layouts/bootstrap-horizontal.gif" width="175"/>
-		</label>
-	</div>
+	<?php } ?>
 	<span class="rsform_clear_both"></span>
 </fieldset>
 
 <fieldset>
 	<legend><?php echo JText::_('RSFP_FORM_HTML_LAYOUT'); ?></legend>
-	<p><button class="rs_button rs_left" type="button" onclick="generateLayout('<?php echo $this->form->FormId; ?>');"><?php echo JText::_('RSFP_GENERATE_LAYOUT'); ?></button> <label for="FormLayoutAutogenerate"><?php echo JText::_('RSFP_AUTOGENERATE_LAYOUT');?> <input type="checkbox" name="FormLayoutAutogenerate" id="FormLayoutAutogenerate" value="1" <?php echo $this->form->FormLayoutAutogenerate ? 'checked="checked"' : ''; ?> onclick="changeFormAutoGenerateLayout('<?php echo $this->form->FormId; ?>');" /></label></p>
+	<table border="0">
+		<tr>
+			<td><button class="rs_button rs_left" type="button" onclick="generateLayout('<?php echo $this->form->FormId; ?>');"><?php echo JText::_('RSFP_GENERATE_LAYOUT'); ?></button></td>
+			<td><?php echo JText::_('RSFP_AUTOGENERATE_LAYOUT');?></td>
+			<td><?php echo $this->lists['FormLayoutAutogenerate']; ?></td>
+		</tr>
+	</table>
 	<table width="100%">
 		<tr>
 			<td valign="top">
-			   <table width="100%" style="clear:both;">
+			   <table width="98%" style="clear:both;">
 					<tr>
 						<td>
 							<textarea rows="20" cols="75" style="width:100%;" class="codemirror-html" name="FormLayout" id="formLayout" <?php echo $this->form->FormLayoutAutogenerate ? 'readonly="readonly"' : '';?>><?php echo $this->escape($this->form->FormLayout); ?></textarea>
